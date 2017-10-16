584bd56b13f42cf953eaf0d955315bf33345df57
Pawel Zakiewicz <pawel.zakiewicz@ft.com>
2017-10-16 18:43:43 +0100

    Tests for ownership in webapps

--- tests/base_container.rb
+++ tests/base_container.rb
@@ -5,12 +5,22 @@ control "servlet_container" do
   desc "Checking..."
 
 
-  describe file('/container-entry.sh') do
-  	it { should exist }
-  	it { should be_file }
-  	it { should be_owned_by 'root' }
-  	its('group') { should eq 'root' }
-  	its('mode') { should cmp '0755' }
+  describe file('/usr/local/tomcat/webapps') do
+    it { should exist }
+    it { should be_directory }
+    it { should be_owned_by 'tomcat' }
+    its('group') { should eq 'tomcat' }
   end
+
+  inspec.command("find /usr/local/tomcat/webapps -type f").stdout.each_line do |filename|
+    describe file(filename.chomp) do
+      it { should be_file }
+      it { should be_owned_by 'tomcat' }
+      # its('mode') { should cmp '0644' }
+      it { should be_readable.by_user('tomcat') }
+      it { should_not be_writable.by('others') }
+    end
+  end
+
   
  end
